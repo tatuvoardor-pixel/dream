f188bb811bdc551eec51943a98f9436242182653
tatuvoardor-pixel <tatuvoardor@gmail.com>
2026-06-28 04:54:42 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,6 @@
-/* SLEEP Service Worker v2.2 */
-const CACHE='sleep-v2.2';
+/* SLEEP Service Worker v2.3 */
+const CACHE='sleep-v2.3';
 const FILES=['./','./index.html','./bio-bridge.js'];
-
-self.addEventListener('install',e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
-});
-
-self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch',e=>{
-  const url=e.request.url;
-  const isCodigo=url.endsWith('.html')||url.endsWith('.js')||url.endsWith('/');
-  if(isCodigo){
-    e.respondWith(
-      fetch(e.request).then(r=>{
-        const c=r.clone();
-        caches.open(CACHE).then(cache=>cache.put(e.request,c));
-        return r;
-      }).catch(()=>caches.match(e.request))
-    );
-  }else{
-    e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)));
-  }
-});
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r;}).catch(()=>caches.match(e.request)));});
